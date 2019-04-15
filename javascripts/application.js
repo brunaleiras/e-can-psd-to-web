b0d4b4907ee8c416a01e932e6d2c709aed9ca946
Bruna Leiras <brunaleiras@gmail.com>
2019-04-15 09:30:17 -0300

    adicionando funcionalidade de carrousel com OwlCarousel2

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,15 @@
 $(function () {
-
+	$('.owl-carousel').owlCarousel({
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	})
 });
